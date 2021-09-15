47429d117534a71942af7fb758064d066ca049ad
hiroya-nagakura <nagahiroda@hotmail.co.jp>
2021-09-15 22:48:52 +0900

    add: row

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import './App.css';
+import { Row } from './component/Row';
+import { requests } from './request';
 
 function App() {
   return (
     <div className="App">
+      <Row
+        title="NETFLIX ORIGUINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="DOcumentaries" fetchUrl={requests.fetchDocumentMovies} />
     </div>
   );
 }
